51c62ca755b77255f5bce8c979c9c9fa66ce0927
klayhub-team <hello@klayhub.com>
2021-11-03 11:58:27 +0900

    Launch tokens

--- contracts/klayhub/HnftToken.sol
+++ contracts/klayhub/HnftToken.sol
@@ -3,9 +3,8 @@ pragma solidity >=0.4.25 <0.7.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract hnftToken is ERC20,ERC20Detailed {
-
-    constructor () public ERC20Detailed("hNFT Token", "hNFT", 18) {
-        _mint(msg.sender, 1000000 * (10 ** 18));
+contract hnftToken is ERC20, ERC20Detailed {
+    constructor() public ERC20Detailed("hNFT Token", "hNFT", 18) {
+        _mint(msg.sender, 1000000 * (10**18));
     }
 }
--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -1,13 +1,13 @@
-const kip17 = artifacts.require("KIP17Token");
-const kip7 = artifacts.require("KIP7Token");
+const _time = artifacts.require("KIP17Token");
+const _hub = artifacts.require("KIP7Token");
 
 module.exports = async function (deployer) {
   await deployer.deploy(
-    kip7,
-    "Klayhub governence token",
+    _hub,
+    "Klayhub",
     "HUB",
     "18",
-    "150000000000000000000000000"
+    "1500000000000000000000000000"
   );
-  await deployer.deploy(kip17, "Time is money", "TIME");
+  await deployer.deploy(_time, "TIME NFT", "TIME");
 };
