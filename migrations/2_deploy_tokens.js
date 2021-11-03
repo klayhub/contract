c880127e202561fd8124344d89a09d66ec3eb9a3
klayhub-team <hello@klayhub.com>
2021-11-03 14:18:08 +0900

    refact: Branch

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -2,12 +2,12 @@ const _time = artifacts.require("KIP17Token");
 const _hub = artifacts.require("KIP7Token");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(
-    _hub,
-    "Klayhub",
-    "HUB",
-    "18",
-    "1500000000000000000000000000"
-  );
-  await deployer.deploy(_time, "TIME NFT", "TIME");
+//  deployer.deploy(
+//     _hub,
+//     "Klayhub",
+//     "HUB",
+//     "18",
+//     "1500000000000000000000000000"
+//   );
+ deployer.deploy(_time, "TIME NFT", "TIME");
 };
